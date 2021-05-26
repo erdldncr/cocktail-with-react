ca8b70c763fec455e6874e39ecbd70b574482575
erdldncr <erdldncr@gmail.com>
2021-05-26 11:17:53 +0100

    Updates

---
+++ precache-manifest.b57748029d88a8535ed16111a30d9e2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e2f47d457664b070562a56e508b9ea",
+    "url": "/cocktail-with-react/index.html"
+  },
+  {
+    "revision": "5316012a84f5474db71b",
+    "url": "/cocktail-with-react/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "1fd7ea94aebc841e010d",
+    "url": "/cocktail-with-react/static/js/2.3a6ec1cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktail-with-react/static/js/2.3a6ec1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5316012a84f5474db71b",
+    "url": "/cocktail-with-react/static/js/main.e77fd3f8.chunk.js"
+  },
+  {
+    "revision": "7c22bfaa2bfe52197b9c",
+    "url": "/cocktail-with-react/static/js/runtime-main.b88dda43.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail-with-react/static/media/logo.9a3d2645.svg"
+  }
+]);
